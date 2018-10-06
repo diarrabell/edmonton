b159ead13ab71157f9574baa6427881dee2b2075
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-06 04:10:01 -0400

    Added about page

---
+++ about.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>About the Students</title>
+	</head>
+	<body>
+		<header>
+			<h1> About the Web Development Students</h1>
+			<nav>
+				<ul>
+					<li><a href="index.php">Web Dev Students</a></li>
+					<li><a href="about.php">About These Web Dev Students</a></li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<section>
+				<h2>Jayda Medina</h2>
+				<p>This is filler text about a student! Class of 2021!  Theyre super cool! Below is one of their paragraphs so we have more filler text!</p>
+				<p>During my first semester at the University of Rochester, I took Beginning American Sign Language, CSC170, CSC161, and MTH140. That semester I made Dean’s List and was invited to join the National Society of Collegiate Scholars. The following semester I took CSC171, MTH150, Beginning American Sign Language II, and WRT105 RAGE!. My humanities cluster is American Sign Language and I’m still deciding my Social Science cluster. I am currently deciding between Gender and Women’s studies and Anthropology. This semester I’m taking Intro to Medical Anthropology, which can be used towards either cluster. I’m doing a human and computer interactions track, with the goal of getting a Bachelor of Arts for Computer Science.</p>
+				<img src="images/jayda.jpg" alt="Jayda"/>
+			</section>
+			<section>
+				<h2>Diarra Bell</h2>
+				<p>This is filler text about a student! Class of 2020! Theyre super cool! Below is one of their paragraphs so we have more filler text!</p>
+				<p>My name is Diarra Bell and I’m a junior at the University of Rochester. I am majoring in computer science with a minor in Japanese. When I first came to the U of R, I didn’t know anything about any of those subjects, but I’ve learned a lot in just two years. When I first came to Rochester, my intended major was Physics and Astronomy. I took an intro to computer science class my first semester, and I really enjoyed it. Eventually, I decided to major in computer science. I also still love astronomy, so I’d like to have a career related to that industry in the future.</p>
+				<img src="images/diarra.jpg" alt="Diarra"/>
+			</section>
+			<section>
+				<h2>Margaret Thurston</h2>
+				<p>This is filler text about a student! Class of 2018! Not a comp sci major! Theyre super cool! Below is one of their paragraphs so we have more filler text!</p>
+				<p>College was the first time that I could choose pretty much all of what I wanted to study, and I loved that. Every semester I spent hours and hours looking at the course catalogs, emailing professors for syllabi, sitting in on way more courses than I was going to take, and trying to make decisions (which I’m bad at). I didn’t declare my major until fall of my junior year, when I created my own major (so I never really decided anyway). Although the library has been my second home, I’ve also been pretty involved in a couple clubs (Students for a Democratic Society, Greenspace, and Partners in Reading especially), and kept up violin for the first three years.</p>
+				<img src="images/margaret.jpg" alt="Margaret"/>
+			</section>
+		</main>
+	</body>
+</html>
