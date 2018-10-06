717d2cc872090f9a969bd9c78c948dc940601f67
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-06 04:00:01 -0400

    Made an index

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Web Dev Students</title>
+	</head>
+	<body>
+		<header>
+			<h1> We are Web Development Students!</h1>
+			<nav>
+				<ul>
+					<li><a href="index.php">Web Dev Students</a></li>
+					<li><a href="about.php">About These Web Dev Students</a></li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+			<section>
+				<h2>Jayda</h2>
+				<p>This is filler text about a student! Class of 2021!  Theyre super cool!</p>
+				<img src="images/jayda.jpg" alt="Jayda"/>
+			</section>
+			<section>
+				<img src="images/diarra.jpg" alt="Diarra"/>
+				<h2>Diarra</h2>
+				<p>This is filler text about a student! Class of 2020! Theyre super cool!</p>
+			</section>
+			<section>
+				<h2> Margaret</h2>
+				<p>This is filler text about a student! Class of 2018! Not a comp sci major! Theyre super cool!</p>
+				<img src="images/margaret.jpg" alt="Margaret"/>
+			</section>
+			<section>
+				<p> All of these students have taken CSC170 and are currently taking CSC174. Jayda, Margaret and Diarra welcome you! To learn more about these students, click <strong><a href = "about.php">here</a></strong>.
+			</section>
+		</main>
+	</body>
+</html>
