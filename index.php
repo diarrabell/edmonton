e83c6f8f202939b77169a4171c2ef8f9f00bd1fe
jmedina7 <43220330+jmedina7@users.noreply.github.com>
2018-10-08 16:02:30 -0400

    Finished Index page
    Replaced filler text with actual content

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<header>
-			<h1> We are Web Development Students!</h1>
+			<h1>We are Web Development Students!</h1>
 			<nav>
 				<ul>
 					<li><a href="index.php">Web Dev Students</a></li>
@@ -17,21 +17,21 @@
 		<main>
 			<section>
 				<h2>Jayda</h2>
-				<p>This is filler text about a student! Class of 2021!  Theyre super cool!</p>
+				<p>Jayda Medina is a Computer Science major. She will graduate in 2021. Her extracurriculars include being a member of Strong Jugglers, and TOOP, a student run theatre company.</p>
 				<img src="images/jayda.jpg" alt="Jayda"/>
 			</section>
 			<section>
 				<img src="images/diarra.jpg" alt="Diarra"/>
 				<h2>Diarra</h2>
-				<p>This is filler text about a student! Class of 2020! Theyre super cool!</p>
+				<p>Diarra Bell is a Computer Science major, with a minor in Japanese. She will graduate in 2020. She is a member of The Pact and in her free time, volunteers for Women in Computing.</p>
 			</section>
 			<section>
-				<h2> Margaret</h2>
-				<p>This is filler text about a student! Class of 2018! Not a comp sci major! Theyre super cool!</p>
+				<h2>Margaret</h2>
+				<p>Margaret Thurston was a Development and Sustainability major. Unlike Jayda and Diarra, she's a graduate. She graduated in 2018. She's a member of Students for a Democratic Society, Greenspace, and Partners in Reading.</p>
 				<img src="images/margaret.jpg" alt="Margaret"/>
 			</section>
 			<section>
-				<p> All of these students have taken CSC170 and are currently taking CSC174. Jayda, Margaret and Diarra welcome you! To learn more about these students, click <strong><a href = "about.php">here</a></strong>.
+				<p>All of these students have taken CSC170 and are currently taking CSC174. Jayda, Margaret and Diarra welcome you! To learn more about these students, click <strong><a href = "about.php">here</a></strong>.
 			</section>
 		</main>
 	</body>
