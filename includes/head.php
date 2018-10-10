87f578ae46af57debda7e56bfa84f7d8357e1cde
Margaret Thurston <mthursto@u.rochester.edu>
2018-10-10 00:04:47 -0400

    linked to css stylesheets & tested links

--- includes/head.php
+++ includes/head.php
@@ -2,4 +2,9 @@
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo $title;?></title>
+		<link rel="stylesheet" type="text/css" href="css/1-variables.css">
+		<link rel="stylesheet" type="text/css" href="css/2-base.css">
+		<link rel="stylesheet" type="text/css" href="css/3-semantic-reusable.css">
+		<link rel="stylesheet" type="text/css" href="css/4-appearance.css">
+		<link rel="stylesheet" type="text/css" href="css/5-layout.css">
 	</head>
