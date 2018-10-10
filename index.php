cd87f25a38a6f7c66863ad4dae3c60a043b8c06e
Margaret Thurston <mthursto@u.rochester.edu>
2018-10-10 00:48:57 -0400

    added z-pattern
    I know I have a lot to fix still...

--- index.php
+++ index.php
@@ -3,42 +3,55 @@
 	<?php 
 		$title = "Web Dev Students";
 		include "includes/head.php";?>
-	<body>
+	<body class="homepage">
 		<header>
 			<h1>We are Web Development Students!</h1>
 
 			<button><a href="about.php">Read More!</a></button>
-
-			<img src="images/spider.jpg" alt="Spider Web">
-
-			<!-- This is what was here before:
-				<nav>
-					<ul>
-						<li><a href="index.php">Web Dev Students</a></li>
-						<li><a href="about.php">About The Web Dev Students</a></li>
-					</ul>
-				</nav>
-			-->
 		</header>
+
+		<figure>
+			<img src="images/spider.jpg" alt="Spider Web">
+		</figure>
+		
 		<main>
 			<section>
 				<h2>Jayda</h2>
-				<p>Jayda Medina is a Computer Science major. She will graduate in 2021. Her extracurriculars include being a member of Strong Jugglers, and TOOP, a student run theatre company.</p>
-				<img src="images/jayda.jpg" alt="Jayda"/>
+				<div class="section__content">
+					<p>Jayda Medina is a Computer Science major. She will graduate in 2021. Her extracurriculars include being a member of Strong Jugglers, and TOOP, a student run theatre company.</p>
+					<img src="images/jayda.jpg" alt="Jayda"/>
+				</div><!-- .section__content -->
 			</section>
 			<section>
 				<h2>Diarra</h2>
-				<img src="images/diarra.jpg" alt="Diarra"/>
-				<p>Diarra Bell is a Computer Science major, with a minor in Japanese. She will graduate in 2020. She is a member of The Pact and in her free time, volunteers for Women in Computing.</p>
+				<div class="section__content">
+					<img src="images/diarra.jpg" alt="Diarra"/>
+					<p>Diarra Bell is a Computer Science major, with a minor in Japanese. She will graduate in 2020. She is a member of The Pact and in her free time, volunteers for Women in Computing.</p>
+				</div><!-- .section__content -->
 			</section>
 			<section>
 				<h2>Margaret</h2>
-				<p>Margaret Thurston was a Development and Sustainability major. Unlike Jayda and Diarra, she's a graduate. She graduated in 2018. She's a member of Students for a Democratic Society, Greenspace, and Partners in Reading.</p>
-				<img src="images/margaret.jpg" alt="Margaret"/>
+				<div class="section__content">
+					<p>Margaret Thurston was a Development and Sustainability major. Unlike Jayda and Diarra, she's a graduate. She graduated in 2018. She's a member of Students for a Democratic Society, Greenspace, and Partners in Reading.</p>
+					<img src="images/margaret.jpg" alt="Margaret"/>
+				</div><!-- .section__content -->
 			</section>
-			<section>
-				<p>All of these students have taken CSC170 and are currently taking CSC174. Jayda, Margaret and Diarra welcome you! To learn more about these students, click <strong><a href = "about.php">here</a></strong>.
-			</section>
+			<section class="bottom-row">
+				<p>All of these students have taken CSC170 and are currently taking CSC174. Jayda, Margaret and Diarra welcome you!</p>
+				<button><a href = "about.php">Learn More</a></button>
+			</section><!-- .bottom-row -->
 		</main>
 	</body>
 </html>
+
+
+
+
+<!-- Stuff we cut out:
+	<nav>
+		<ul>
+			<li><a href="index.php">Web Dev Students</a></li>
+			<li><a href="about.php">About The Web Dev Students</a></li>
+		</ul>
+	</nav>
+-->
